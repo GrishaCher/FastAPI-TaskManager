c2f57acc71c9a5b1a2ee848b3baebb7bf16b4e6b
GrishaCher <grish.chernikow2016@yandex.ru>
2025-07-26 01:07:03 +0300

    add UsersDB model

---
+++ backend/app/models/users.py
@@ -0,0 +1,16 @@
+from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy.orm import relationship
+from app.models.base import Base
+from datetime import datetime
+
+class User(Base):  # Модель пользователей для таблицы в БД
+
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, nullable=False)
+    email = Column(String, nullable=False)
+    created_at = Column(DateTime, default=datetime.now)
+
+    tasks=relationship("Task", back_populates="owner")
+    groups = relationship("Group", secondary="user_groups") 
