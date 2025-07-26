99694997c8267dd204f6e67399a687fc9b2c0d14
GrishaCher <grish.chernikow2016@yandex.ru>
2025-07-26 18:54:39 +0300

    fix config.py

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,12 +1,11 @@
-from pydantic import BaseSettings, PostgresDsn
-
+from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
-    POSTGRES_URL: PostgresDsn = "postgresql+asyncpg://user:pass@localhost:5432/db"
+    POSTGRES_URL: str 
     DEBUG: bool = False
     SECRET_KEY: str 
     ALGORITHM:str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
-
+    
     class Config:
         env_file = ".env"
 
