c49c55fabe64654972ebc20355a427e2d4edae4e
GrishaCher <grish.chernikow2016@yandex.ru>
2025-07-27 21:28:46 +0300

    mod config.py

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,10 +1,20 @@
 from pydantic_settings import BaseSettings
+from pydantic import computed_field
 class Settings(BaseSettings):
-    POSTGRES_URL: str 
+    #POSTGRES_URL: str 
     DEBUG: bool = False
     SECRET_KEY: str 
     ALGORITHM:str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+    DB_USER:str
+    DB_PASSWORD:str
+    DB_HOST:str
+    DB_PORT:str
+    DB_NAME:str
+    @computed_field
+    @property
+    def sqlalchemyURL(self)->str:
+        return (f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASSWORD}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}")
     
     class Config:
         env_file = ".env"
