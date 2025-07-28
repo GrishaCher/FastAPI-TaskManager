984ae9df8033d4bee7e155cd964117fc41a054eb
GrishaCher <grish.chernikow2016@yandex.ru>
2025-07-28 23:58:51 +0300

    mod session.pt

--- backend/app/core/session.py
+++ backend/app/core/session.py
@@ -1,10 +1,16 @@
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
+from app.core.config import settings
 
-def get_db_sessionmaker(postgres_url: str):
-    engine = create_async_engine(postgres_url,echo=False)
-    return sessionmaker(
-        bind=engine,
-        class_=AsyncSession,
-        expire_on_commit=False
-    )
+engine = create_async_engine(settings.sqlalchemyURL,echo=False)
+async_session=sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
+from typing import AsyncGenerator
+
+async def get_session() -> AsyncGenerator[AsyncSession, None]:
+    """Получить асинхронную сессию для работы с базой данных.
+
+    Yields:
+        AsyncSession: Асинхронная сессия SQLAlchemy.
+    """
+    async with async_session() as session:
+        yield session
--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,6 @@
 from app.core.config import settings
 from app.core.session import get_db_sessionmaker
 from fastapi import FastAPI
-print(settings.POSTGRES_URL)
-LocalSession=get_db_sessionmaker(postgres_url=settings.POSTGRES_URL)
+print(settings.sqlalchemyURL)
+LocalSession=get_db_sessionmaker(postgres_url=settings.sqlalchemyURL)
 #app=FastAPI()
