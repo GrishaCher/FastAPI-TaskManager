a175a608d195a6214b735f5ca6fcb25814f12adf
GrishaCher <grish.chernikow2016@yandex.ru>
2025-08-02 23:19:08 +0300

    rewrite user schemas

--- backend/app/schemas/user.py
+++ backend/app/schemas/user.py
@@ -1,22 +1,30 @@
 from app.schemas.base import BaseModelWithConfig
+from app.schemas.token import Token
 from datetime import datetime
 from typing import Optional
 from pydantic import EmailStr #, HttpUrl 
 
 class UserLogin(BaseModelWithConfig):
-    email: str
+    email: EmailStr
     password: str
 class UserRegister(UserLogin):
     username:str
+class UserResponse(BaseModelWithConfig):
+    email:EmailStr
+    username: str
+class UserWithTime(UserResponse):
+    created_at:datetime=None
+    update_at:datetime=None
 
 class UserInUpdate(UserRegister):
     username: Optional[str] = None
     email: Optional[EmailStr] = None
-    password: Optional[str] = None
+    password: Optional[str] = None 
     created_at: Optional[datetime]= None
     #image: Optional[HttpUrl] = None   Для фото профиля
-class UserWithToken(UserRegister):
-    token:str
-class UserResponse(BaseModelWithConfig):
-    user: UserWithToken
+class UserWithToken(UserResponse):
+    token:Token
+
+# class UserWithTokenResponse(BaseModelWithConfig):
+#     user: UserWithToken
     
