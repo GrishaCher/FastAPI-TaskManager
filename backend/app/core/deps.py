8542f9ee77d7ff3ba919cac11a6d4fe4a906cfa9
GrishaCher <grish.chernikow2016@yandex.ru>
2025-08-02 23:25:46 +0300

    error verification in deps.py

--- backend/app/core/deps.py
+++ backend/app/core/deps.py
@@ -2,21 +2,27 @@ from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
 from jose import JWTError
 from app.core.security import auth_service
-from app.core.config import settings
 from app.db.models.users import User
 from app.db.session import get_session
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/login")
 oauth2_refresh_scheme = OAuth2PasswordBearer(tokenUrl="auth/refresh")
-async def get_current_user(db: AsyncSession = Depends(get_session), token: str = Depends(oauth2_scheme)):
+async def get_current_user(db: AsyncSession = Depends(get_session), token: str = Depends(oauth2_scheme))->User:
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
         detail="Не удалось подтвердить учетные данные",
         headers={"WWW-Authenticate": "Bearer"},
     )
-    payload = auth_service.decode_token(token)
-    user = db.query(User).filter(User.email == payload["sub"]).first()
-    if not user:
+    try:
+        payload = auth_service.decode_token(token)
+    except JWTError as e:
+        raise credentials_exception
+    get_email=payload["sub"]
+    if get_email is None:
+        raise credentials_exception
+    user = await db.scalar(select(User).where(User.email == get_email)).first()
+    if user is None:
         raise credentials_exception
     return user
