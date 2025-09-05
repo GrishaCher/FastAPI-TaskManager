3ff0a991b792b47840af9b5a3124787c96a17507
GrishaCher <grish.chernikow2016@yandex.ru>
2025-09-06 01:47:15 +0300

    add constraints for async_session

--- backend/app/db/session.py
+++ backend/app/db/session.py
@@ -2,7 +2,11 @@ from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
 from app.core.config import settings
 
-engine = create_async_engine(settings.sqlalchemyURL,echo=False)
+engine = create_async_engine(settings.sqlalchemyURL,echo=False,
+    pool_recycle=300,  # Пересоздавать соединения каждые 300 сек
+    pool_pre_ping=True,  # Проверять соединение перед использованием
+    pool_timeout=30,  # Ждать свободного соединения до 30 сек
+    )
 async_session=sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
 from typing import AsyncGenerator
 
